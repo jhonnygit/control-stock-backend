009c32fb790eb36c8c3a475c62c0c02449c5e957
jhonnygit <jhonprograma@gmail.com>
2023-08-10 16:22:07 -0400

    configuracion de puerto

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     res.status(200).send('<h1>Hola mundo con nodemon</h1>')
 });
 
-app.listen(4000,()=>{
-    console.log('Servidor escuchando en el puerto 4000')
+const PORT=process.env.PORT || 4000
+
+app.listen(PORT,()=>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
 })
