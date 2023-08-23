1bd0515b7045eb718b383cd004e6a39aca9d2c23
jhonnygit <jhonprograma@gmail.com>
2023-08-23 15:40:32 -0400

    formulario

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express=require('express')
 const mongoose=require('mongoose')
-const path=require('path');
+const cors=require('cors')
+const path=require('path')
 require('dotenv').config()
 
 const app=express()
+
+app.use(cors())
 
 const PORT=process.env.PORT
 
@@ -33,14 +36,23 @@ const Product=mongoose.model('Product',productSchema)
 app.use(express.json())
 
 app.post('/api/v1/products',(req,res) => {
+
+    if(!req.body.name){
+        res.status(400).json({
+            ok:false,
+            message:"El campo Nombre es obligatorio"
+        })
+        return
+    }
+
+
     const newProduct=new Product(req.body)
     newProduct
     .save()
     .then((result)=>{
         res.status(201).json({ok:true})
     })
-    .catch((err)=>console.log(err))
-    
+    .catch((err)=>console.log(err))     
 })
 
 app.use(express.static(path.join(__dirname,'public')))
